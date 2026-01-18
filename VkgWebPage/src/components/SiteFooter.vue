9c4576cb48b00fecdb86a0692f34289f94533039
jramso <josue.rsou2@gmail.com>
2026-01-18 13:23:12 -0300

    feat: adding support total batle website...

--- VkgWebPage/src/components/SiteFooter.vue
+++ VkgWebPage/src/components/SiteFooter.vue
@@ -5,5 +5,8 @@
       All rights reserved. Crafted with valor and honor.<br>
       <a href="https://github.com/jramso">Created by Josué R Souza. | BR Hakon</a>
     </p>
+    <p class="text-sm text-medieval-gold font-medieval">
+      Support website ScoreWarrior: <a href="https://scorewarrior.helpshift.com/hc/en/3-total-battle/" class="underline">scorewarrior.helpshift.com</a>
+    </p>
   </footer>
 </template>
